58942ce6d9730073a00de69c58bd217108afc72b
samuel0973 <samuel.bru.mezquita@estudiantat.upc.edu>
2026-05-04 18:56:35 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,69 +1,52 @@
 // a proposal for main.c ..... just to inspire you
-#include <xc.h>
-#include "config.h"
-#include "GLCD.h"
-
-#define _XTAL_FREQ 8000000 // needed for __delay_ms function
-
-typedef enum{
-    Ready=0,
-    Running,
-    Stopped
-} state_t;
-// global Variables
-const char * s1 = "L4B GLCD\n";
-const char * s2 = "--------\n";
-
-const char * s_nom = "Mar Cabrelles\n"; // Canvia-ho pel teu
-const char * s2_nom = "Samuel Bru Mezquita\n"; // Canvia-ho pel teu
-
-void tic(void)
-{
-    //Coses a fer periòdicament
-}
-// interrupt service routine for handling high priority Timer0
-void interrupt rutinaAtencioInterrupcio (void) 
-
-    if (timer0Flag) {
-        tic();
-    }
-    // falling edge detection and debouncing
-    ??? inputDetector(·) {
-
-    }
-// initialize PORTs, timer0 and basic PIC resources
-void configPIC() {
-    ANSELE = 0x00;
-    ANSELB = 0x00;
-    ANSELD = 0x00;
-
-    TRISE = 0x01;
-
-    PORTB = 0x00;
-    PORTD = 0x00;
-}
+#include "myHeaders.h"
 
 void main(void) {
 
     configPIC();
+    GLCDinit();
+    clearGLCD(0,7,0,127);
+    
 
-    writeTxt(2, 8, s1);     // "L4A GLCD" al centre
-    writeTxt(3, 8, s2);     // "L4A GLCD" al centre
-    writeTxt(4, 4, s_nom);  // El teu nom a sota
-    writeTxt(5, 4, s2_nom);  // El teu nom a sota
+    writeTxt(2, 8, s1);     
+    writeTxt(3, 8, s2);     
+    writeTxt(4, 4, s_nom);  
+    writeTxt(5, 4, s2_nom);  
 
-    __delay_ms(1000);       // Esperem 1 segon
-    clearGLCD(0,7,0,127);   // Tornem a esborrar per al bucle principal
+    __delay_ms(1000);       
+    clearGLCD(0,7,0,127);   
 
-
-    while (1) { // MAIN LOOP starts here
-        if ( inputDetector(·) ) { // check raising edge
+    writeTxt(0,2,get_state_char(state));
+    
+    paint_values();
+    draw_progress_bar(5,36,0);
+   
+    while (1) { 
+        if (inputDetector()) { // check raising edge
             switch(state) {
-            // depending on the current state, handle the transition
-
+               // depending on the current state, handle the transition
+	       case Ready: 
+		  state = Running;
+		  transition = 1;
+		  increment = 1;
+		  break;
+	       
+	       case Running:
+		  state = Stopped;
+		  transition = 1;
+		  increment = 0;
+		  break;
+	       
+	       case Stopped:
+		  state = Ready;
+		  transition = 1;
+		  increment = 0;
+		  reset_values();
+		  break;
             }
         }   
-    updateGLCD(·); // show things on the GLCD smartly!
+	 updateGLCD(); // show things on the GLCD smartly!
 
-    }// MAIN LOOP ends here
+    }
 }
+
